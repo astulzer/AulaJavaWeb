c5b862e430b63805a3740e6eeb4d7ece8803ce19
Alex Stulzer <astulzer@hotmail.com>
2021-03-02 20:18:26 -0300

    primeiro commit

---
+++ src/main/java/br/com/semac/ProjetoSenacApplication.java
@@ -0,0 +1,13 @@
+package br.com.semac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSenacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSenacApplication.class, args);
+	}
+
+}
